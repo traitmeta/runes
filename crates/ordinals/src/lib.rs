9bea6aba844f81c628bf4e9d2fac4565af03dac9
traitmeta <traitmeta@gmail.com>
2024-04-25 20:10:41 +0800

    add isncription id in ordinals

--- crates/ordinals/src/lib.rs
+++ crates/ordinals/src/lib.rs
@@ -59,3 +59,19 @@ mod sat_point;
 mod spaced_rune;
 mod terms;
 pub mod varint;
+mod inscription_id;
+
+
+#[macro_export]
+macro_rules! assert_matches {
+  ($expression:expr, $( $pattern:pat_param )|+ $( if $guard:expr )? $(,)?) => {
+    match $expression {
+      $( $pattern )|+ $( if $guard )? => {}
+      left => panic!(
+        "assertion failed: (left ~= right)\n  left: `{:?}`\n right: `{}`",
+        left,
+        stringify!($($pattern)|+ $(if $guard)?)
+      ),
+    }
+  }
+}
